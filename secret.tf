6bf4b0b1c4e4b1ed70b220f9a1356392fa078c72
mogowda <86524927+mogowda@users.noreply.github.com>
2021-08-19 19:18:04 +0530

    Update secret.tf

--- secret.tf
+++ secret.tf
@@ -1,10 +1,10 @@
-resource "aws_secretsmanager_secret" "example" {
-  name                    = "rds_admin"
-  description             = "RDS Admin password"
+resource "aws_secretsmanager_secret" "mydb" {
+  name                    = "rdsadmin"
+  description             = "admin password"
   recovery_window_in_days = 7
 }
 
 resource "aws_secretsmanager_secret_version" "secret" {
-  secret_id     = aws_secretsmanager_secret.example.id
+  secret_id     = aws_secretsmanager_secret.mydb.id
   secret_string = random_password.password.result
 }
